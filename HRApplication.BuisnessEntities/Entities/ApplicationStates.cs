f61f14de54ea4365bfe3174dbb508b66321159c7
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-25 10:12:06 +0200

    #111_CreateClassesFromDatabase

---
+++ HRApplication.BuisnessEntities/Entities/ApplicationStates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRApplication.DataAccess.Entities
+{
+    public partial class ApplicationStates
+    {
+        public ApplicationStates()
+        {
+            ApplicationStatusHistory = new HashSet<ApplicationStatusHistory>();
+        }
+
+        public Guid Id { get; set; }
+        public string StatusName { get; set; }
+
+        public virtual ICollection<ApplicationStatusHistory> ApplicationStatusHistory { get; set; }
+    }
+}
---
+++ HRApplication.BuisnessEntities/Entities/ApplicationStatusHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRApplication.DataAccess.Entities
+{
+    public partial class ApplicationStatusHistory
+    {
+        public Guid Id { get; set; }
+        public Guid ApplicationId { get; set; }
+        public Guid ApplicationStateId { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Applicationss Application { get; set; }
+        public virtual ApplicationStates ApplicationState { get; set; }
+    }
+}
