b5ba0e78c066e1d6e3460ef41281d4c36bc9ed68
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-26 11:27:06 +0200

    #101_Add job offer view model

---
+++ HRApplication/Models/JobOffer/JobOfferViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRApplication.WWW.Models.JobOffer
+{
+    public class JobOfferViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Typ kontraktu")]
+        public Guid? ContractTypeId { get; set; }
+
+        [Display(Name = "Część etatu")]
+        public bool PartTimeWork { get; set; }
+
+        [Display(Name = "Godziny tygodniowo")]
+        public decimal? HoursPerWeek { get; set; }
+
+        [Display(Name = "Data zakończenia ogłoszenia")]
+        public DateTime? EndDate { get; set; }
+
+        [Display(Name = "Stanowisko")]
+        public string Position { get; set; }
+    }
+}
