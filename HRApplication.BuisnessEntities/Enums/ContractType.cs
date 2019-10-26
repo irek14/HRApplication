09a2d72ab83aacfe17815a31e148ab5612ba3412
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-26 13:23:03 +0200

    #103 Mock data scirpt & enums added

---
+++ HRApplication.BuisnessEntities/Enums/ContractType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BuisnessEntities.Enums
+{
+    public enum ContractType
+    {
+        Labor = 1,
+        Order = 2,
+        B2B = 3,
+        Specific = 4
+    }
+}
