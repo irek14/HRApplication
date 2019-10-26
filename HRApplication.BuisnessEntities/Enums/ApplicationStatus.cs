2012e46483a40c249820e776c0216d20a35c27c2
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-26 13:23:03 +0200

    #103 Mock data scirpt & enums added

---
+++ HRApplication.BuisnessEntities/Enums/ApplicationStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BuisnessEntities.Enums
+{
+    public enum ApplicationStatus
+    {
+        Approved = 1,
+        Rejected = 2,
+        New = 3
+    }
+}
