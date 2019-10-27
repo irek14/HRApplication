ef566bdaa134a9e3d40c7cee77ecad5f0c0a888a
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-27 22:31:47 +0100

    #70_Create application controller, interface & service

---
+++ HRApplication.BusinessLogic/Interfaces/IApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BusinessLogic.Interfaces
+{
+    public interface IApplicationService
+    {
+
+    }
+}
---
+++ HRApplication.BusinessLogic/Services/ApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using HRApplication.BusinessLogic.Interfaces;
+using HRApplication.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BusinessLogic.Services
+{
+    public class ApplicationService: IApplicationService
+    {
+
+        private readonly HRAppDBContext _context;
+
+        public ApplicationService(HRAppDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
