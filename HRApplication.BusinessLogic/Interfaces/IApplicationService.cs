fc43120e990c539b2d5e24f40a87457274af31fd
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-27 23:08:06 +0100

    #70_Get all job offers function works

--- HRApplication.BusinessLogic/Interfaces/IApplicationService.cs
+++ HRApplication.BusinessLogic/Interfaces/IApplicationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRApplication.DataAccess.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace HRApplication.BusinessLogic.Interfaces
 {
     public interface IApplicationService
     {
-
+        List<Offers> GetAllJobOffers();
     }
 }
