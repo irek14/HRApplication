482ff46fed1398069c4063558fdc9fbe37799273
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-10-28 09:52:16 +0100

    #134_Create job details view model

---
+++ HRApplication/Models/Application/DetailsJobOfferViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRApplication.WWW.Models.JobOffer
+{
+    public class DetailsJobOfferViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Typ umowy")]
+        public Guid ContractTypeId { get; set; }
+
+        [Display(Name = "Pensja")]
+        public string Salary { get; set; }
+
+        [Display(Name = "Część etatu")]
+        public bool PartTimeWork { get; set; }
+
+        [Display(Name = "Godziny tygodniowo")]
+        public decimal HoursPerWeek { get; set; }
+
+        [Display(Name = "Data zakończenia ogłoszenia")]
+        public DateTime? EndDate { get; set; }
+
+        [Display(Name = "Stanowisko")]
+        public string Position { get; set; }
+    }
+}
