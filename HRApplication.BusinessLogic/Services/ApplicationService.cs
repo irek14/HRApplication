e0890af56a365589b641d01c015653dfb29bf368
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-11-09 11:52:49 +0100

    #75_Server side logic to add new application

--- HRApplication.BusinessLogic/Services/ApplicationService.cs
+++ HRApplication.BusinessLogic/Services/ApplicationService.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using HRApplication.BuisnessEntities.Enums;
+using System.Threading.Tasks;
 
 namespace HRApplication.BusinessLogic.Services
 {
@@ -16,6 +19,47 @@ namespace HRApplication.BusinessLogic.Services
         public ApplicationService(HRAppDBContext context)
         {
             _context = context;
+        }
+
+        public async Task<bool> AddNewApplication(Guid JobOfferId, IFormFile CV)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    ApplicationStatusHistory statusHistory = new ApplicationStatusHistory
+                    {
+                        Id = Guid.NewGuid()
+                    };
+
+                    Applications application = new Applications
+                    {
+                        CreatedById = Guid.Parse("17496B8A-8E4E-4E8A-8099-101998018B03"),
+                        CreateOn = DateTime.Now,
+                        CvfileName = CV.FileName,
+                        Id = Guid.NewGuid(),
+                        OfferId = JobOfferId,
+                        ApplicationStatusHistoryId = statusHistory.Id,
+                        CurrentApplicationStateName = ApplicationStatusesData.applicationStatusesIds[(int)ApplicationStatus.New].name
+                    };
+
+                    statusHistory.ApplicationStateId = ApplicationStatusesData.applicationStatusesIds[(int)ApplicationStatus.New].id;
+                    statusHistory.ApplicationId = application.Id;
+                    statusHistory.Date = application.CreateOn;
+
+                    _context.Applicationss.Add(application);
+                    _context.ApplicationStatusHistory.Add(statusHistory);
+
+                    _context.SaveChanges();
+
+                    transaction.Commit();
+                }
+                catch(Exception e)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public List<Offers> GetAllJobOffers()
