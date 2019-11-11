583009aaa5bc2d902d2bd7840d91ac2b9da63e79
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-11-11 10:28:21 +0100

    #55_Create admin controller,service & application view model

---
+++ HRApplication.BusinessLogic/Services/AdminService.cs
@@ -0,0 +1,16 @@
+﻿using HRApplication.BusinessLogic.Interfaces;
+using HRApplication.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BusinessLogic.Services
+{
+    public class AdminService : IAdminService
+    {
+        public List<Applications> GetAllApplications()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HRApplication/Models/AdminPanel/TableApplicationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRApplication.WWW.Models.AdminPanel
+{
+    public class TableApplicationViewModel
+    {
+        public Guid ApplicationId { get; set; }
+        public string JobOfferTitle { get; set; }
+        public string ApplicationState { get; set; }
+        public string UserName { get; set; }
+        public DateTime ApplicationDate { get; set; }
+    }
+}
