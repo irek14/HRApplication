16d1dcb2f6628adf74f28a37f5fd25a3aad2364f
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-11-11 11:00:17 +0100

    #56_Get all application function works

--- HRApplication.BusinessLogic/Services/AdminService.cs
+++ HRApplication.BusinessLogic/Services/AdminService.cs
@@ -1,16 +1,33 @@
 ﻿using HRApplication.BusinessLogic.Interfaces;
 using HRApplication.DataAccess.Entities;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRApplication.BusinessLogic.Services
 {
     public class AdminService : IAdminService
     {
+        private readonly HRAppDBContext _context;
+        private IConfiguration _configuration;
+
+        public AdminService(HRAppDBContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
         public List<Applications> GetAllApplications()
         {
-            throw new NotImplementedException();
+            var result = _context.Applicationss
+                        .Include(x => x.CreatedBy)
+                        .Include(x => x.Offer)
+                        .Include(x => x.Offer.CreatedBy);
+
+            return result.ToList();
         }
     }
 }
--- HRApplication/Controllers/AdminPanelController.cs
+++ HRApplication/Controllers/AdminPanelController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HRApplication.BusinessLogic.Interfaces;
+using HRApplication.DataAccess.Entities;
+using HRApplication.WWW.Models.AdminPanel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRApplication.WWW.Controllers
@@ -18,7 +20,24 @@ namespace HRApplication.WWW.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Applications> applications = _adminService.GetAllApplications();
+            List<TableApplicationViewModel> result = new List<TableApplicationViewModel>();
+
+            foreach (Applications app in applications)
+            {
+                TableApplicationViewModel model = new TableApplicationViewModel
+                {
+                    ApplicationId = app.Id,
+                    ApplicationDate = app.CreateOn,
+                    ApplicationState = app.CurrentApplicationStateName,
+                    HRMemberName = app.Offer.CreatedBy.FirstName + " " + app.Offer.CreatedBy.LastName,
+                    JobOfferTitle = app.Offer.Title,
+                    UserName = app.CreatedBy.FirstName + " " + app.CreatedBy.LastName
+                };
+                result.Add(model);
+            }
+
+            return View(result);
         }
     }
 }
