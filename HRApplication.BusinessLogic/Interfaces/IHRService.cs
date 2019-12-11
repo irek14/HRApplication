8c2332c92ac68bf38c7bcf005f3f04167c2ba313
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-12-11 20:58:18 +0100

    #108_Get all application for hr member function works

--- HRApplication.BusinessLogic/Interfaces/IHRService.cs
+++ HRApplication.BusinessLogic/Interfaces/IHRService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRApplication.WWW.Models.HR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace HRApplication.BusinessLogic.Interfaces
 {
     public interface IHRService
     {
-
+        List<TableApplicationViewModel> GetAllApplications(Guid HRMemberId, DateTime? dateSince, DateTime? dateTo, string jobOffer, string person);
     }
 }
---
+++ HRApplication.BusinessLogic/Models/HR/TableApplicationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRApplication.WWW.Models.HR
+{
+    public class TableApplicationViewModel
+    {
+        public Guid ApplicationId { get; set; }
+        [Display(Name = "Tytuł")]
+        public string JobOfferTitle { get; set; }
+        [Display(Name = "Status aplikacji")]
+        public string ApplicationState { get; set; }
+        [Display(Name = "Aplikant")]
+        public string UserName { get; set; }
+        [Display(Name = "Ostatnia zmiana statusu")]
+        public DateTime ApplicationDate { get; set; }
+    }
+}
