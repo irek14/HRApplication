d0867b785e800f1c55d2e1cf3802c0bea1dbc8ce
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-12-11 20:58:18 +0100

    #108_Get all application for hr member function works

--- HRApplication/Controllers/HRController.cs
+++ HRApplication/Controllers/HRController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using HRApplication.BusinessLogic.Interfaces;
 using HRApplication.DataAccess.Entities;
-using HRApplication.WWW.Models.AdminPanel;
+using HRApplication.WWW.Models.HR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRApplication.WWW.Controllers
@@ -26,13 +26,15 @@ namespace HRApplication.WWW.Controllers
         [HttpGet]
         public PagingViewModel GetApplications(int pageSize, int pageNumber, DateTime? dateSince, DateTime? dateTo, string jobOffer, string person)
         {
+            //TODO: Get ID from claims
+            Guid hrMemberId = Guid.Parse("DACB7B3D-780B-44E8-9F68-7F62200DEAE3");
 
-            //List<TableApplicationViewModel> applications = _adminService.GetAllApplications(dateSince, dateTo, jobOffer, person);
+            List<TableApplicationViewModel> applications = _hrService.GetAllApplications(hrMemberId,dateSince, dateTo, jobOffer, person);
             PagingViewModel result = new PagingViewModel();
 
-            //result.Applications = applications.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-            //result.TotalRecord = applications.Count();
-            //result.TotalPage = (result.TotalRecord / pageSize) + ((result.TotalRecord % pageSize) > 0 ? 1 : 0);
+            result.Applications = applications.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            result.TotalRecord = applications.Count();
+            result.TotalPage = (result.TotalRecord / pageSize) + ((result.TotalRecord % pageSize) > 0 ? 1 : 0);
 
             return result;
         }
