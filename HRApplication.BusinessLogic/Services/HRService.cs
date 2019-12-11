96ca5d7dc6c2af667ca358b68d1d0fc81ef51753
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-12-11 20:45:49 +0100

    #107_Create HR Controller with empty service

---
+++ HRApplication.BusinessLogic/Services/HRService.cs
@@ -0,0 +1,11 @@
+﻿using HRApplication.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRApplication.BusinessLogic.Services
+{
+    public class HRService : IHRService
+    {
+    }
+}
