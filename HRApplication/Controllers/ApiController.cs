af857d0f07c4fdc3ea1b80828ef13e8a3b4abca5
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-12-28 16:01:11 +0100

    #52_Add LogIn action

---
+++ HRApplication/Controllers/ApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+//using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRApplication.WWW.Controllers
+{
+    public class ApiController : Controller
+    {
+        public async Task<IActionResult> LogIn()
+        {
+            bool test1 = User.IsInRole("Admin");
+
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("SignIn", "AzureADB2C//Account");
+
+            (User.Identity as ClaimsIdentity).AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+            ClaimsPrincipal claims = User as ClaimsPrincipal;
+
+            //await HttpContext.Authentication.SignOutAsync(HttpContext);
+            //await HttpContext.Authentication.SignInAsync(DefaultAuthenticationTypes.ApplicationCookie, claims);
+
+            //List<Claim> claims = new List<Claim> { new Claim(ClaimTypes.Role, "Admin") };
+            //ClaimsIdentity newIdentity = new ClaimsIdentity(claims);
+            //User.AddIdentity(newIdentity);
+
+            //User.Claims.Append(new Claim(ClaimTypes.Role, "Admin"));
+            
+
+            bool test2 = User.IsInRole("Admin");
+       
+            return RedirectToAction("Index", "Application");
+        }
+    }
+}
