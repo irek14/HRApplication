239be00ab5e1fefebcecadc8750c5fd58943448d
Ireneusz Stanicki <01130611@pw.edu.pl>
2019-12-28 20:49:29 +0100

    #52_Add hardcore admin role, no connection with DB

---
+++ HRApplication/AzureAdB2CAuthenticationBuilderExtensions.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Identity.Client;
+using System.Security.Claims;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HRApplication.WWW;
+using HRApplication.DataAccess.Entities;
+
+namespace HRApplication.BusinessLogic.Services
+{
+    public static class AzureAdB2CAuthenticationBuilderExtensions
+    {
+        public static AuthenticationBuilder AddAzureAdB2C(this AuthenticationBuilder builder)
+            => builder.AddAzureAdB2C(_ =>
+            {
+            });
+
+        public static AuthenticationBuilder AddAzureAdB2C(this AuthenticationBuilder builder, Action<AzureAdB2COptions> configureOptions)
+        {
+            builder.Services.Configure(configureOptions);
+            builder.Services.AddSingleton<IConfigureOptions<OpenIdConnectOptions>, OpenIdConnectOptionsSetup>();
+            builder.AddOpenIdConnect();
+            return builder;
+        }
+
+        public class OpenIdConnectOptionsSetup : IConfigureNamedOptions<OpenIdConnectOptions>
+        {
+            private readonly HRAppDBContext _context;
+            public OpenIdConnectOptionsSetup(IOptions<AzureAdB2COptions> b2cOptions)
+            {
+                AzureAdB2COptions = b2cOptions.Value;
+                //_context = context;
+            }
+
+            public AzureAdB2COptions AzureAdB2COptions { get; set; }
+
+            public void Configure(string name, OpenIdConnectOptions options)
+            {
+                options.ClientId = AzureAdB2COptions.ClientId;
+                options.Authority = AzureAdB2COptions.Authority;
+                options.UseTokenLifetime = true;
+                options.TokenValidationParameters = new TokenValidationParameters() { NameClaimType = "name" };
+                options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnRedirectToIdentityProvider = OnRedirectToIdentityProvider,
+                    OnRemoteFailure = OnRemoteFailure,
+                    OnAuthorizationCodeReceived = context =>
+                    {
+                        return OnAuthorizationCodeReceived(context);
+                    }
+                };
+            }
+
+            public void Configure(OpenIdConnectOptions options)
+            {
+                Configure(Options.DefaultName, options);
+            }
+
+            public Task OnRedirectToIdentityProvider(RedirectContext context)
+            {
+                var defaultPolicy = AzureAdB2COptions.DefaultPolicy;
+                if (context.Properties.Items.TryGetValue(AzureAdB2COptions.PolicyAuthenticationProperty, out var policy) &&
+                    !policy.Equals(defaultPolicy))
+                {
+                    context.ProtocolMessage.Scope = OpenIdConnectScope.OpenIdProfile;
+                    context.ProtocolMessage.ResponseType = OpenIdConnectResponseType.IdToken;
+                    context.ProtocolMessage.IssuerAddress = context.ProtocolMessage.IssuerAddress.ToLower().Replace(defaultPolicy.ToLower(), policy.ToLower());
+                    context.Properties.Items.Remove(AzureAdB2COptions.PolicyAuthenticationProperty);
+                }
+                else if (!string.IsNullOrEmpty(AzureAdB2COptions.ApiUrl))
+                {
+                    context.ProtocolMessage.Scope += $" offline_access {AzureAdB2COptions.ApiScopes}";
+                    context.ProtocolMessage.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+                }
+                return Task.FromResult(0);
+            }
+
+            public Task OnRemoteFailure(RemoteFailureContext context)
+            {
+                context.HandleResponse();
+                // Handle the error code that Azure AD B2C throws when trying to reset a password from the login page 
+                // because password reset is not supported by a "sign-up or sign-in policy"
+                if (context.Failure is OpenIdConnectProtocolException && context.Failure.Message.Contains("AADB2C90118"))
+                {
+                    // If the user clicked the reset password link, redirect to the reset password route
+                    context.Response.Redirect("/Session/ResetPassword");
+                }
+                else if (context.Failure is OpenIdConnectProtocolException && context.Failure.Message.Contains("access_denied"))
+                {
+                    context.Response.Redirect("/");
+                }
+                else
+                {
+                    context.Response.Redirect("/Home/Error?message=" + Uri.EscapeDataString(context.Failure.Message));
+                }
+                return Task.FromResult(0);
+            }
+
+            public async Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedContext context)
+            {
+                // Use MSAL to swap the code for an access token
+                // Extract the code from the response notification
+                var code = context.ProtocolMessage.Code;
+
+                string signedInUserID = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+                IConfidentialClientApplication cca = ConfidentialClientApplicationBuilder.Create(AzureAdB2COptions.ClientId)
+                    .WithB2CAuthority(AzureAdB2COptions.Authority)
+                    .WithRedirectUri(AzureAdB2COptions.RedirectUri)
+                    .WithClientSecret(AzureAdB2COptions.ClientSecret)
+                    .Build();
+                new MSALStaticCache(signedInUserID, context.HttpContext).EnablePersistence(cca.UserTokenCache);
+
+                if (context.Principal != null)
+                {
+
+                    var claims = context.Principal.Identities.First().Claims;
+                    Users au = new Users();
+                    var v = claims.FirstOrDefault(x => ClaimTypes.Email == x.Type || x.Type == "email" || x.Type == "emails");
+                    var v1 = claims.FirstOrDefault(x => ClaimTypes.GivenName == x.Type);
+                    var v2 = claims.FirstOrDefault(x => ClaimTypes.Surname == x.Type);
+                    au.Email = v != null ? v.Value : "";
+                    au.FirstName = v1 != null ? v1.Value : "";
+                    au.LastName = v2 != null ? v2.Value : "";
+                    //var first = _context.Users.Where(x => x.Email == au.Email).Include(i => i.Role).FirstOrDefault();
+
+
+
+                    //_context.Users.Select(x=>x).Include().FirstOrDefault(x => x.Email == au.Email);
+
+                    //if (first == null)
+                    {
+                        //au.Role = _context.Roles.First(x => x.RoleName == Roles.User.ToString());
+                        //_context.Users.Add(au);
+                        //_context.SaveChanges();
+                        //first = au;
+                    }
+                    context.Principal.Identities.First().AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+
+                    var p = context.Principal.Identities.First();
+                }
+
+                try
+                {
+                    AuthenticationResult result = await cca.AcquireTokenByAuthorizationCode(AzureAdB2COptions.ApiScopes.Split(' '), code)
+                        .ExecuteAsync();
+                    context.HandleCodeRedemption(result.AccessToken, result.IdToken);
+                }
+                catch
+                {
+                    //TODO: Handle
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ HRApplication/Controllers/SessionController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json;
+
+namespace HRApplication.WWW.Controllers
+{
+    public class SessionController : Controller
+    {
+        public SessionController(IOptions<AzureAdB2COptions> b2cOptions)
+        {
+            AzureAdB2COptions = b2cOptions.Value;
+        }
+
+        public AzureAdB2COptions AzureAdB2COptions { get; set; }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var redirectUrl = Url.Action(nameof(SessionController.postUserClaims), "Session");
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl },
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
+        [HttpGet]
+        public IActionResult postUserClaims()
+        {
+            //ApplicationUser user = new ApplicationUser();
+            //foreach (var v in User.Claims)
+            //{
+            //    if (v.Type.Contains("email"))
+            //        user.Email = v.Value;
+            //    if (v.Type.Contains("givenname"))
+            //        user.Name = v.Value;
+            //    if (v.Type.Contains("surname"))
+            //        user.Lastname = v.Value;
+            //    if (v.Type.Contains("nameidentifier"))
+            //        user.NameIdentifier = v.Value;
+            //    user.RoleId = (int)Roles.HRUser;
+            //}
+
+            (User.Identity as ClaimsIdentity).AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+            //var us = User;
+            //user.GetRole = Roles.User;
+
+            //if (_userService.Add(user) != null) _userService.Save();
+
+            return Redirect(Url.Action(nameof(ApiController.LogIn), "Home"));
+        }
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            var redirectUrl = Url.Action(nameof(ApiController.LogIn), "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.ResetPasswordPolicyId;
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult EditProfile()
+        {
+            var redirectUrl = Url.Action(nameof(ApiController.LogIn), "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.EditProfilePolicyId;
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            var callbackUrl = Url.Action(nameof(SignedOut), "Session", values: null, protocol: Request.Scheme);
+            return SignOut(new AuthenticationProperties { RedirectUri = callbackUrl },
+                CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignedOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction(nameof(ApiController.LogIn), "Home");
+            }
+
+            return View();
+        }
+    }
+}
